ded9560f39559d573a6f568f97214259603a7956
patelshivani3 <shivpatel3001@gmail.com>
2023-02-01 17:26:43 +0530

    Adding Permutation Of String

---
+++ DataStructureAlgorithms/Permutation/Recursion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithms.Permutation
+{
+    internal class Recursion
+    {
+        public static void Permutation_Using_Recursion_Method(String str, int startIndex, int endIndex)
+        {
+            if (startIndex == endIndex)
+                Console.Write(str + "  ");
+            else
+            {
+                for (int i = startIndex; i <= endIndex; i++)
+                {
+                    str = Swap(str, startIndex, i);
+                    Permutation_Using_Recursion_Method(str, startIndex + 1, endIndex);
+                    str = Swap(str, startIndex, i);
+                }
+            }
+        }
+        public static string Swap(String a,int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}
---
+++ DataStructureAlgorithms/Program.cs
@@ -0,0 +1,25 @@
+﻿using DataStructureAlgorithms.Permutation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "";
+            Console.WriteLine("\nEnter the String for Permutation : ");
+            string s = Console.ReadLine();
+            Console.WriteLine("\nPossible Strings Using Iteration Method : ");
+            Iteration.Permutation_Using_Iteration_Method(s, output);
+            Console.WriteLine("\nPossible Strings Using Recursion Method :");
+            int n = s.Length;
+            Recursion.Permutation_Using_Recursion_Method(s, 0, n - 1);
+            Console.ReadLine();
+        }
+    }
+}
